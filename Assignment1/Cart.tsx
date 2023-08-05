593e1f522209ca7524229436716899db25938eaf
Ilham Sheikh <ilhamsheikh@Ilhams-MacBook-Pro.local>
2023-08-04 22:12:37 -0400

    Debugged the cart screen code

--- Assignment1/Cart.tsx
+++ Assignment1/Cart.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 const CartScreen = () => {
   const items = [
@@ -9,10 +10,10 @@ const CartScreen = () => {
 
   const renderItems = () => {
     return items.map((item) => (
-      <div key={item.id}>
-        <span>{item.name}</span>
-        <span>${item.price}</span>
-      </div>
+      <View key={item.id} style={styles.itemContainer}>
+        <Text>{item.name}</Text>
+        <Text>${item.price}</Text>
+      </View>
     ));
   };
 
@@ -25,13 +26,60 @@ const CartScreen = () => {
   };
 
   return (
-    <div style={{ background: "linear-gradient(to bottom, #ffffff, #f2f2f2)" }}>
-      <h2 style={{ color: "green" }}>Cart Screen</h2>
-      <div>{renderItems()}</div>
-      <h3>Total: ${calculateTotal()}</h3>
-      <button style={{ background: "linear-gradient(to bottom, #4CAF50, #398A46)", color: "white" }}>Checkout</button>
-    </div>
+    <View style={styles.container}>
+      <Text style={styles.heading}>Cart Screen</Text>
+      <View style={styles.content}>{renderItems()}</View>
+      <Text style={styles.total}>Total: ${calculateTotal()}</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Place Order</Text>
+      </TouchableOpacity>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f2f2f2',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'green',
+    marginVertical: 20,
+  },
+  content: {
+    flex: 1, 
+    width: '100%', 
+  },
+  itemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: 'gray',
+  },
+  total: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 20,
+  },
+  button: {
+    backgroundColor: '#398A46',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    marginTop: 20,
+    marginBottom: 40,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
 
 export default CartScreen;
